f72eff4b87c4dd8bd7f70ec9251696f29aeaf721
Scott Ewing <Scew5145@colorado.edu>
2021-12-18 16:01:28 -0700

    CityGenerator poisson disk sampling for placement

--- Assets/Scripts/Overworld/Generators/Region/CityGenerator.cs
+++ Assets/Scripts/Overworld/Generators/Region/CityGenerator.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class CityGenerator
 {
-  int numCities = 8;
-  public readonly RegionGeneratorData regionData; // Note that only the reference value is readonly, but the array itself is good to be modified
+  public int numCities = 16;
+  public RegionGeneratorData regionData; // Note that only the reference value is readonly, but the array itself is good to be modified
 
   public Texture2D generatedTexture;
-  public List<ZoneGeneratorData> landBasedZones;
+  public List<ZoneGeneratorData> validUntriedZones;
   public List<ZoneGeneratorData> cities;
+  private int maxCityPlacementAttempts = 10;
+  public float minDistanceBetweenCities = 4;
   public CityGenerator(RegionGeneratorData _regionData)
   {
     regionData = _regionData;
     Random.InitState(regionData.seed);
-    landBasedZones = new List<ZoneGeneratorData>();
+    validUntriedZones = new List<ZoneGeneratorData>();
     cities = new List<ZoneGeneratorData>();
     generatedTexture = new Texture2D(regionData.regionDimensions.x * regionData.zoneDimensions.x, regionData.regionDimensions.y * regionData.zoneDimensions.y);
   }
@@ -29,49 +31,83 @@ public class CityGenerator
       {
         if (regionData.allZoneData[x, y].tags.Contains("land"))
         {
-          landBasedZones.Add(regionData.allZoneData[x, y]);
+          validUntriedZones.Add(regionData.allZoneData[x, y]);
         }
       }
     }
-  }
-
-
-  // a transformation mostly for the simplification of code, so that I can read this in a month without going crazy
-  // I don't know if this is actually slower, or if the extra you get from not having to load each zone as individually is faster, but frankly, I don't care ;.;
-  // TODO: if worldgen things get slow, try removing this
-  public float[,] CreateHeightMap()
-  {
-    float[,] newHeightMap = new float[regionData.regionDimensions.x * regionData.zoneDimensions.x, regionData.regionDimensions.y * regionData.zoneDimensions.y];
-    for (int overworldY = 0; overworldY < regionData.regionDimensions.y; overworldY++)
-    {
-      for (int overworldX = 0; overworldX < regionData.regionDimensions.x; overworldX++)
-      {
-        for (int zoneY = 0; zoneY < regionData.zoneDimensions.y; zoneY++)
-        {
-          for (int zoneX = 0; zoneX < regionData.zoneDimensions.x; zoneX++)
-          {
-            newHeightMap[overworldX + zoneX, overworldY + zoneY] = regionData.allZoneData[overworldX, overworldY].heightMap[zoneX, zoneY];
-          }
-        }
-      }
-    }
-    return newHeightMap;
   }
   public void Generate()
   {
     GatherLandBasedZones();
+    if(numCities > validUntriedZones.Count)
+    {
+      Debug.LogError("Not enough space to place all towns! Zone count: " + validUntriedZones.Count + " City count: " + numCities);
+      return;
+    }
     List<ZoneGeneratorData> tempCities = new List<ZoneGeneratorData>();
+    List<ZoneGeneratorData> triedZones = new List<ZoneGeneratorData>();
     for (int city = 0; city < numCities; city++)
     {
-      int index = Random.Range(0, landBasedZones.Count);
-      tempCities.Add(landBasedZones[index]);
-      landBasedZones.RemoveAt(index);
+      int attempts = 0;
+      // Attempt poisson disk sampling until we run out of attempts.
+      while (attempts <= maxCityPlacementAttempts)
+      {
+        if(validUntriedZones.Count == 0)
+        {
+          Debug.LogWarning("Ran out of space to place cities with good spacing! D:");
+          break;
+        }
+        int index = Random.Range(0, validUntriedZones.Count);
+        // Debug.Log("Placing: " + validUntriedZones[index].OverworldCoordinates.x + " " + validUntriedZones[index].OverworldCoordinates.y);
+        float distanceFromCity = float.MaxValue;
+        for (int placedIndex = 0; placedIndex < tempCities.Count; placedIndex ++)
+        {
+          ZoneGeneratorData placedCity = tempCities[placedIndex];
+          float distanceToPlaced = Vector2Int.Distance(validUntriedZones[index].OverworldCoordinates, placedCity.OverworldCoordinates);
+          // Debug.Log("Checking: " + placedCity.OverworldCoordinates.x + " " + placedCity.OverworldCoordinates.y);
+          // Debug.Log(distanceToPlaced);
+          if (distanceToPlaced < distanceFromCity)
+          {
+            distanceFromCity = distanceToPlaced;
+          }
+        }
+        // Debug.Log(distanceFromCity);
+        if (distanceFromCity <= minDistanceBetweenCities)
+        {
+          // Debug.LogWarning("Failed a placement");
+        }
+        else
+        {
+          // Found a valid cell, add it to the list of cities
+          tempCities.Add(validUntriedZones[index]);
+          validUntriedZones.RemoveAt(index);
+          break;
+        }
+        triedZones.Add(validUntriedZones[index]);
+        validUntriedZones.RemoveAt(index);
+        attempts += 1;
+      }
+      // If we fell all the way out of the attempt cycle without finding one, we'll add it afterwards.
+      // The next city won't try the ones we've already attempted (because they're removed from validUntriedZones)
     }
-    landBasedZones.AddRange(tempCities); // Re-add the cities for future operations
+    validUntriedZones.AddRange(triedZones); // Re-add the cities for future operations
 
-    float[,] heightmap = CreateHeightMap();
-    Dictionary<Vector2Int, int> maskedVoronoiMap = CreateMaskedVoronoiMap(heightmap, tempCities);
+    if(tempCities.Count < numCities)
+    {
+      // If we don't have as many cities as we hoped to generate, we place the remainder naively (no disk samples, full random)
+      // We KNOW there's enough zones to assign because of the previous check (numCities > validUntriedZones.Count), so as long as we aren't
+      // placing two cities on top of one another we can ensure this random placement will work.
+      Debug.Log("Placing extra cities. Placed: " + tempCities.Count + " Remaining: " + (numCities - tempCities.Count));
+      for (int city = 0; city < numCities - tempCities.Count; city++)
+      {
+        int index = Random.Range(0, validUntriedZones.Count);
+        tempCities.Add(validUntriedZones[index]);
+        validUntriedZones.RemoveAt(index);
+      }
+    }
 
+    // ... and finally, finish restocking the validUntriedZones in case we need them later
+    validUntriedZones.AddRange(tempCities);
 
     cities = tempCities;
     Color[] pix = new Color[generatedTexture.width * generatedTexture.height]; // This should be a greyscale texture to go fast, but... L A Z Y
@@ -103,6 +139,31 @@ public class CityGenerator
     generatedTexture.Apply();
   }
 
+
+  // CreateHeightMap and CreateMaskedVoronoiMap are currently unused, because I'm using possion disk sampling for city placement atm.
+  // These were for a alternate workflow, where we divide the region into subregions, and then move the cities to the center of each subregion.
+  // See Lloyd Relaxation (though it's not a perfect 1-to-1 for this technique) for more info
+  // a transformation mostly for the simplification of code, so that I can read this in a month without going crazy
+  // I don't know if this is actually slower, or if the extra you get from not having to load each zone as individually is faster, but frankly, I don't care ;.;
+  // TODO: if worldgen things get slow, try removing this
+  public float[,] CreateHeightMap()
+  {
+    float[,] newHeightMap = new float[regionData.regionDimensions.x * regionData.zoneDimensions.x, regionData.regionDimensions.y * regionData.zoneDimensions.y];
+    for (int overworldY = 0; overworldY < regionData.regionDimensions.y; overworldY++)
+    {
+      for (int overworldX = 0; overworldX < regionData.regionDimensions.x; overworldX++)
+      {
+        for (int zoneY = 0; zoneY < regionData.zoneDimensions.y; zoneY++)
+        {
+          for (int zoneX = 0; zoneX < regionData.zoneDimensions.x; zoneX++)
+          {
+            newHeightMap[overworldX + zoneX, overworldY + zoneY] = regionData.allZoneData[overworldX, overworldY].heightMap[zoneX, zoneY];
+          }
+        }
+      }
+    }
+    return newHeightMap;
+  }
   public Dictionary<Vector2Int, int> CreateMaskedVoronoiMap(float[,] heightMap, List<ZoneGeneratorData> citiesToUse)
   {
     Dictionary<Vector2Int, int> voronoiMap = new Dictionary<Vector2Int, int>();
